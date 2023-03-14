3d5eaa1445e0f6a94da3e0142d8576b2e74f7d94
junaid.alam <junaid.alam@tenx.ai>
2023-03-14 15:59:44 +0500

    Updates to lock filter function and flock metadata view

--- src/test/java/Test/Ancera/DBValidations/DSCoccidiaOPGView.java
+++ src/test/java/Test/Ancera/DBValidations/DSCoccidiaOPGView.java
@@ -1,75 +1,114 @@
 package Test.Ancera.DBValidations;
 
 import MiscFunctions.DB_Config_DW;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static Config.BaseTest.saveResult;
+import static Test.Ancera.DBValidations.View_Queries.*;
 
 
 public class DSCoccidiaOPGView extends DB_Config_DW {
 
-    @Test(enabled= true, priority= 1)
-    public void RowCompare() throws InterruptedException, IOException, SQLException {
+    public static void viewsDataCompare(String oldView, String newView) throws SQLException, InterruptedException, IOException {
+        try {
+            SoftAssert softAssert = new SoftAssert();
 
-        SoftAssert softAssert = new SoftAssert();
+            String query1 = oldView;
+            ResultSet rs1 = getStmt().executeQuery(query1);
+            Thread.sleep(3000);
+            List<String> data = new ArrayList<String>();
 
-    //    String query1 = "Select count(status) as countOldView from COCCIDA_OUTPUT where Sample_ID like '202212%' ";
-        String query1 = "Select count(T_Run_ID) as countOldView from DS_COCCIDIA_OPG_FLOCK_METADATA";
-        ResultSet rs1 = getStmt().executeQuery(query1);
-        rs1.next();
-        int countOldView = rs1.getInt(1);
-        System.out.println("Count1: "+countOldView);
+            ResultSetMetaData rsmd = rs1.getMetaData();
+            int column_count = rsmd.getColumnCount();  //get column count
 
-        String query2 = "Select count(status) as countNewView from COCCIDA_OUTPUT where Sample_ID like '202212%' ";
-        ResultSet rs2 = getStmt().executeQuery(query2);
-        rs2.next();
-        int countNewView = rs2.getInt(1);
-        System.out.println("Count2: "+countNewView);
-
-        softAssert.assertEquals(countNewView, countOldView);
-        softAssert.assertAll();
-    }
-
-    @Test(enabled= true, priority= 1)
-    public void DataCompare() throws InterruptedException, IOException, SQLException {
-
-        SoftAssert softAssert = new SoftAssert();
-
-        String query1 = "Select Top 10 * from COCCIDA_OUTPUT where Sample_ID like '202212%' ";
-        ResultSet rs1 = getStmt().executeQuery(query1);
-        rs1.next();
-
-        String query2 = "Select * from COCCIDA_OUTPUT where Sample_ID like '202212%' ";
-        ResultSet rs2 = getStmt().executeQuery(query2);
-        rs2.next();
-
-// Compare the data in the two tables
-        while (rs1.next() && rs2.next()) {
-            // Compare the values in each column
-            for (int i=1;i<10;i++) {
-                System.out.print(rs1.getInt(i));
-                System.out.println();
-                System.out.print(rs2.getInt(i));
-
-               if (rs1.getInt(i) != (rs2.getInt(i))) {
-                    System.out.println("Data in tables is not the same");
-                    break;
+            while (rs1.next()) {
+                for (int i = 1; i <= column_count; i++) {
+                    String columnValue = rs1.getString(i);
+                    data.add(columnValue);    //add all table data to List 'data'
                 }
             }
+
+            String query2 = newView;
+            ResultSet rs2 = getStmt().executeQuery(query2);
+            Thread.sleep(3000);
+            List<String> datanew = new ArrayList<String>();
+
+            while (rs2.next()) {
+                for (int i = 1; i <= column_count; i++) {
+                    String columnValue = rs2.getString(i);
+                    datanew.add(columnValue);   //add all table data to List 'datanew'
+                }
+            }
+
+            int total_tabledata = column_count * datanew.size() / column_count;  //get total number of values in table
+
+            for (int z = 1; z <= total_tabledata; z++) {
+                softAssert.assertEquals(datanew.get(z - 1), data.get(z - 1), "Data not matching in row " + (z - 1) / column_count);
+            }
+            softAssert.assertAll();
         }
-
-        // If the while loop finished without breaking, the data is the same
-        System.out.println("Data in tables is the same");
-
-
-    //    softAssert.assertEquals(countNewView, countOldView);
-        softAssert.assertAll();
+        catch (AssertionError er) {
+            saveResult(ITestResult.FAILURE, new Exception(er));
+        }
     }
 
+
+    @Test(enabled = true, priority = 1)
+    public void RowCompare() throws InterruptedException, IOException, SQLException {
+        try {
+            SoftAssert softAssert = new SoftAssert();
+
+           // String query1 = countRowsView + oldViewName;
+            String query1 = getAllRowsCountQuery(oldViewName);
+            ResultSet rs1 = getStmt().executeQuery(query1);
+            rs1.next();
+            int countOldView = rs1.getInt(1);
+            System.out.println("Count1: " + countOldView);
+
+          //  String query2 = countRowsView + newViewName;
+            String query2 = getAllRowsCountQuery(newViewName);
+            ResultSet rs2 = getStmt().executeQuery(query2);
+            rs2.next();
+            int countNewView = rs2.getInt(1);
+            System.out.println("Count2: " + countNewView);
+
+            softAssert.assertEquals(countNewView, countOldView);
+            softAssert.assertAll();
+        } catch (AssertionError er) {
+            saveResult(ITestResult.FAILURE, new Exception(er));
+        }
+    }
+
+
+    @Test(enabled = true, priority = 3)
+    public static void AllDataCompare() throws SQLException, InterruptedException, IOException {
+        viewsDataCompare(getAllDataQuery(oldViewName), getAllDataQuery(newViewName));
+    }
+
+    @Test(enabled = true, priority = 4)
+    public static void DataCompareNoCyclingConfig() throws SQLException, InterruptedException, IOException {
+        viewsDataCompare(getNoCyclingConfigQuery(oldViewName), getNoCyclingConfigQuery(newViewName));
+    }
+
+    @Test(enabled = true, priority = 5)
+    public static void OutOfIntervalRange() throws SQLException, InterruptedException, IOException {
+        viewsDataCompare(getOutofIntervalRangeQuery(oldViewName), getOutofIntervalRangeQuery(newViewName));
+    }
+
+    @Test(enabled = true, priority = 6)
+    public static void NoFlockAssociation() throws SQLException, InterruptedException, IOException {
+        viewsDataCompare(getNoFlockAssociationQuery(oldViewName), getNoFlockAssociationQuery(newViewName));
+    }
 
 
     @AfterTest
