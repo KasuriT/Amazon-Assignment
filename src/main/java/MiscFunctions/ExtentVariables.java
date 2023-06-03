26c7009719966ef689845265d2eff2a7918ab8f3
KasuriT <kasurit1990@gmail.com>
2023-06-03 15:14:22 +0500

    test

---
+++ src/main/java/MiscFunctions/ExtentVariables.java
@@ -0,0 +1,16 @@
+package MiscFunctions;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+public class ExtentVariables {
+
+    public static ExtentTest test;
+    public static ExtentTest results;
+    public static ExtentReports extent;
+
+    public static String Results = "Results";
+
+}
